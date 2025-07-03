85f9fb28609517ac7cafc9c90af37025aac636dd
sahilbhola14 <sbhola@umich.edu>
2025-07-03 09:59:50 -0400

    chore(plot): Plot with and without ProMiNo

--- scripts/oneDCorr/plot.py
+++ scripts/oneDCorr/plot.py
@@ -3,31 +3,56 @@ import matplotlib.pyplot as plt
 
 plt.style.use("../journal.mplstyle")
 
-data = torch.load("oneDCorr_results_mfFlow.pt")
+data_mfFlow = torch.load("oneDCorr_results_mfFlow.pt")
+data = torch.load("oneDCorr_results.pt")
+
+field_mfFlow = data_mfFlow["field"]
 field = data["field"]
-residual = data["residual"]
-domain = data["domain"].ravel()
+residual_mfFlow = data_mfFlow["residual"]
+domain = data_mfFlow["domain"].ravel()
 
 fig, ax = plt.subplots(2, 3, figsize=(15, 10), sharex=True, sharey=True)
 ax = ax.flatten()
 for ii in range(6):
-    LF_field = field.get("LF_field")[ii]
-    HF_field = field.get("HF_field")[ii]
+    LF_field_mfFlow = field_mfFlow.get("LF_field")[ii]
+    HF_field_mfFlow = field_mfFlow.get("HF_field")[ii]
+    # With ProMiNO
+    Prediction_mfFlow = field_mfFlow.get("Prediction")[ii]
+    mean_Prediction_mfFlow = Prediction_mfFlow.mean(dim=0)
+    std_Prediction_mfFlow = Prediction_mfFlow.std(dim=0)
+    # Without ProMiNO
     Prediction = field.get("Prediction")[ii]
     mean_Prediction = Prediction.mean(dim=0)
     std_Prediction = Prediction.std(dim=0)
-    ax[ii].plot(domain, LF_field, label="Low-fidelity", color="blue")
-    ax[ii].plot(domain, HF_field, label="High-fidelity", color="k")
-    ax[ii].plot(domain, mean_Prediction, label="ProMiNO", color="red", linestyle="--")
+
+    ax[ii].plot(domain, LF_field_mfFlow, label="Low-fidelity", color="blue")
+    ax[ii].plot(domain, HF_field_mfFlow, label="High-fidelity", color="k")
+
+    ax[ii].plot(
+        domain, mean_Prediction_mfFlow, label="ProMiNO", color="red", linestyle="--"
+    )
+    ax[ii].plot(domain, mean_Prediction, label="ProNO", color="green", linestyle="--")
+
     ax[ii].fill_between(
         domain,
-        mean_Prediction - std_Prediction,
-        mean_Prediction + std_Prediction,
+        mean_Prediction_mfFlow - std_Prediction_mfFlow,
+        mean_Prediction_mfFlow + std_Prediction_mfFlow,
         color="red",
         alpha=0.2,
         label=r"$\pm \sigma$",
         linestyle="--",
     )
+
+    ax[ii].fill_between(
+        domain,
+        mean_Prediction - std_Prediction,
+        mean_Prediction + std_Prediction,
+        color="green",
+        alpha=0.2,
+        label=r"$\pm \sigma$",
+        linestyle="--",
+    )
+
     ax[ii].set_xlabel(r"$x$")
     ax[ii].set_ylabel(r"$w(a)$")
     if ii == 0:
