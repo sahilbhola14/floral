ba920a30786ba5c273cbd1b99f59a3dd02a9bc5d
sahilbhola14 <sbhola@umich.edu>
2025-07-14 09:41:43 -0400

    feat(gp): Add GP plot script

--- scripts/oneDCorr/plot.py
+++ scripts/oneDCorr/plot.py
@@ -5,6 +5,7 @@ plt.style.use("../journal.mplstyle")
 
 data_mfFlow = torch.load("oneDCorr_results_mfFlow.pt")
 data = torch.load("oneDCorr_results.pt")
+data_gp = torch.load("oneDCorr_GP_results.pt")
 
 field_mfFlow = data_mfFlow["field"]
 field = data["field"]
@@ -25,6 +26,11 @@ for ii in range(6):
     mean_Prediction = Prediction.mean(dim=0)
     std_Prediction = Prediction.std(dim=0)
 
+    # Prediction GP
+    Prediction = data_gp.get("Prediction")
+    mean_Prediction_gp = Prediction.get("mean")[ii]
+    std_Prediction_gp = Prediction.get("std")[ii]
+
     ax[ii].plot(domain, LF_field_mfFlow, label="Low-fidelity", color="blue")
     ax[ii].plot(domain, HF_field_mfFlow, label="High-fidelity", color="k")
 
@@ -32,6 +38,7 @@ for ii in range(6):
         domain, mean_Prediction_mfFlow, label="ProMiNO", color="red", linestyle="--"
     )
     ax[ii].plot(domain, mean_Prediction, label="ProNO", color="green", linestyle="--")
+    ax[ii].plot(domain, mean_Prediction_gp, label="GP", color="orange", linestyle="--")
 
     ax[ii].fill_between(
         domain,
@@ -48,6 +55,16 @@ for ii in range(6):
         mean_Prediction - std_Prediction,
         mean_Prediction + std_Prediction,
         color="green",
+        alpha=0.2,
+        label=r"$\pm \sigma$",
+        linestyle="--",
+    )
+
+    ax[ii].fill_between(
+        domain,
+        mean_Prediction_gp - std_Prediction_gp,
+        mean_Prediction_gp + std_Prediction_gp,
+        color="orange",
         alpha=0.2,
         label=r"$\pm \sigma$",
         linestyle="--",
