8126973dc5a8d0c561c6459a318d7fcff5902790
Sahil Bhola <sbhola@umich.edu>
2025-07-15 19:07:00 -0400

    chore: remove legacy functions

--- examples/poisson/run_GP.py
+++ examples/poisson/run_GP.py
@@ -2,14 +2,12 @@
 """
 Gaussian Process Regression for Poisson equation
 """
-import numpy as np
 import torch
 import argparse
 import gpytorch
 from tqdm import tqdm
 from omegaconf import OmegaConf
 from mfFlow.utils import (
-    n2t,
     printer,
     GPDataModule,
     InferenceGP,
@@ -71,49 +69,6 @@ def train_GP(model):
         # Valication step
         val_loss = model.val_step(mll)
         pbar.set_postfix({"train_loss": train_loss.item(), "val_loss": val_loss.item()})
-
-
-@torch.no_grad()
-def infer_GP(model, likelihood, statistics):
-    # Get the conditions
-    test_data = np.load(config.data.test_data_path, allow_pickle=True)
-    condition = n2t(test_data.get("condition", None))
-    LF_field = n2t(test_data.get("LF_field", None))
-    HF_field = n2t(test_data.get("HF_field", None))
-    B, Nc = condition.shape
-    domain = (
-        n2t(test_data.get("domain", None)).unsqueeze(0).repeat(condition.shape[0], 1, 1)
-    )
-    in_features = torch.cat([condition.view(-1, 1), domain.view(-1, 1)], dim=-1).float()
-    # Normalize the input features
-    in_features = (in_features - statistics["in_features_train_mean"]) / statistics[
-        "in_features_train_std"
-    ]
-    # Set to eval mode
-    model.eval()
-    likelihood.eval()
-    # Infer
-    with gpytorch.settings.fast_pred_var():
-        pred = likelihood(model(in_features))
-        pred_mean = pred.mean
-        pred_std = pred.stddev
-    # Denormalize the output
-    pred_mean = (
-        pred_mean.unsqueeze(-1) * statistics["out_features_train_std"]
-        + statistics["out_features_train_mean"]
-    )
-    pred_std = pred_std.unsqueeze(-1) * statistics["out_features_train_std"]
-    # Reshape
-    pred_mean = pred_mean.view(B, Nc)
-    pred_std = pred_std.view(B, Nc)
-    # Results
-    results = {
-        "LF_field": LF_field,
-        "HF_field": HF_field,
-        "Prediction": {"mean": pred_mean, "std": pred_std},
-    }
-    # Save results
-    torch.save(results, "test.pt")
 
 
 if __name__ == "__main__":
