81829e4a19450b6151070ef483a7c272b6661221
sahilbhola14 <sbhola@umich.edu>
2025-07-22 19:38:56 -0400

    wip: Add hyperparameters tuning selection

--- examples/oneDCorr/run_FLOREN.py
+++ examples/oneDCorr/run_FLOREN.py
@@ -4,9 +4,11 @@ Flow-Matching Residual Embedded Nerual Opeartor for One-dimensional correlation
 benchmark.
 """
 import torch
+import wandb
 import torch.nn as nn
 import argparse
 import pytorch_lightning as L
+import yaml
 from omegaconf import OmegaConf
 from mfFlow.utils import (
     printer,
@@ -27,21 +29,40 @@ parser.add_argument(
     default="config_FLOREN.yml",
     help="Path to the configuration file.",
 )
+
 args = parser.parse_args()
 config = OmegaConf.load(args.config)
-printer(f"Running oneDCorr with configuration: {args.config}")
-printer(f"Using mfFlow: {config.mfFlow} and job name: {config.job_name}")
-printer(
-    f"Number of samples: {config.data.high_fidelity.n_samples} "
-    f"sensors: {config.data.high_fidelity.n_sensors}"
-)
-
 
 torch.set_float32_matmul_precision("medium")  # for tensor cores
 
 
-def get_data_module():
-    """Get the data module for the oneDCorr problem."""
+def print_header(config: dict):
+    """Print the Header
+    Args:
+        config (dict): Configuration dictionary parameters.
+    """
+    printer("==" * 50)
+    printer(f"Running oneDCorr with FLOREN ({config.job_name})")
+    printer(f"Configuration file: {args.config}")
+    printer(f"Tune hyperparameters: {config.tune_hyperparameters}")
+    printer(f"Multi-fidelity Flow: {config.mfFlow}")
+    printer(f"Number of samples: {config.data.high_fidelity.n_samples}")
+    printer(f"Number of sensors: {config.data.high_fidelity.n_sensors}")
+    printer("==" * 50)
+
+
+def build_data_module(
+    config: dict, hp_config: wandb.sdk.wandb_config.Config | dict = None
+):
+    """Get the data module for the oneDCorr problem.
+    config (dict): Configuration dictionary containing data parameters.
+    hp_config (dict): Hyperparameter configuration dictionary.
+    """
+    # data loader config
+    dataloader_config = dict(config.dataloader.copy())
+    # set batch size from hyperparameter config
+    dataloader_config["batch_size"] = hp_config["batch_size"]
+
     data_module = OpDataModule(
         nx=config.data.nx,
         nc=config.data.nc,
@@ -51,7 +72,7 @@ def get_data_module():
         n_samples=config.data.high_fidelity.n_samples,
         n_sensors=config.data.high_fidelity.n_sensors,
         mfFlow=config.mfFlow,
-        dataloader_config=config.dataloader,
+        dataloader_config=dataloader_config,
         test_data_path=config.data.test_data_path,
     )
 
@@ -61,19 +82,72 @@ def get_data_module():
     return data_module
 
 
+def build_checkpointer(config: dict):
+    """Get the checkpointer for saving and loading model checkpoints."""
+    # get the checkpointer
+    ckp_save_path = config.checkpoint_save_path
+    checkpointer = get_checkpointer(
+        ckp_save_path + "/mfFlow" if config.mfFlow else ckp_save_path
+    )
+    return checkpointer
+
+
+def build_trainer(
+    config: dict,
+    hp_config: wandb.sdk.wandb_config.Config | dict = None,
+    checkpointer=None,
+):
+    """Get the trainer for training the model.
+    Args:
+        config (dict): Configuration dictionary containing training parameters.
+        hp_config (wandb.sdk.wandb_config.Config | dict): Hyperparameter configuration
+        checkpointer: Checkpointer for saving and loading model checkpoints.
+    Returns:
+        trainer (pytorch_lightning.Trainer): Trainer object for training the model.
+    """
+    # logger name
+    logger_name = config.logger_name.lower().strip()
+    # train config
+    train_config = dict(config.train.copy())
+    train_config["max_epochs"] = hp_config["max_epochs"]
+
+    trainer = get_trainer(
+        checkpointer=checkpointer,
+        logger_name=logger_name + "_mfFlow" if config.mfFlow else logger_name,
+        train_config=train_config,
+    )
+
+    return trainer
+
+
 class ResFlow(Flow, L.LightningModule):
     """Class for the residual flow model."""
 
-    def __init__(self, config: dict):
-        super(ResFlow, self).__init__()
-        self.save_hyperparameters()
+    def __init__(
+        self, config: dict, hp_config: wandb.sdk.wandb_config.Config | dict = None
+    ):
+        # Initialize the Flow and LightningModule
+        Flow.__init__(self, hp_config=hp_config)
+        L.LightningModule.__init__(self)
+
+        # convert to dict for saving
+        # config_dict = OmegaConf.to_container(config, resolve=True)
+        hp_config_dict = dict(hp_config)
+
+        # save the config and hyperparameter config
+        self.save_hyperparameters({"config": config, "hp_config": hp_config_dict})
+
         self.config = config
         self.nx = self.config.data.nx
         self.nc = self.config.data.nc
         self.nd = self.config.data.nd
 
         # flow config
-        self.flow_config = self.config.flow
+        self.flow_config = self.config.flow.copy()
+        self.flow_config["time_emb_freq"] = hp_config["time_emb_freq"]
+        self.flow_config["latent_dim"] = hp_config["latent_dim"]
+        self.flow_config["num_centers"] = hp_config["num_centers"]
+
         self.sig_min = self.flow_config.sig_min
         self.time_emb_freq = self.flow_config.time_emb_freq
         self.latent_dim = self.flow_config.latent_dim
@@ -136,26 +210,31 @@ class ResFlow(Flow, L.LightningModule):
         return out
 
 
-if __name__ == "__main__":
-    # get the data module
-    data_module = get_data_module()
-    # get the checkpointer
-    ckp_save_path = config.checkpoint_save_path
-    checkpointer = get_checkpointer(
-        ckp_save_path + "/mfFlow" if config.mfFlow else ckp_save_path
+def train_model(hp_config: dict = None):
+    """Train the model using the specified configuration and hyperparameters.
+    Returns:
+        best_model_path (str): Path to the best model checkpoint after training.
+    """
+    if config.tune_hyperparameters:
+        # initialize wandb with the hyperparameter config
+        wandb.init()
+        hp_config = wandb.config
+    else:
+        assert (
+            hp_config is not None
+        ), "Hyperparameter config must be provided for training."
+    # data module
+    data_module = build_data_module(config=config, hp_config=hp_config)
+    # checkpointer
+    checkpointer = build_checkpointer(config=config)
+    # get trainer
+    trainer = build_trainer(
+        config=config, hp_config=hp_config, checkpointer=checkpointer
     )
-    # get the trainer
-    logger_name = config.logger_name
-    trainer = get_trainer(
-        checkpointer=checkpointer,
-        logger_name=logger_name + "_mfFlow" if config.mfFlow else logger_name,
-        train_config=config.train,
-    )
-    # Model
-    model = ResFlow(config)
+    # model
+    model = ResFlow(config=config, hp_config=hp_config)
     model.apply(init_weights)
-
-    # Check if checkpoint is being loaded
+    # load checkpoint if specified
     if config.checkpoint_load_path is not None:
         check_path(config.checkpoint_load_path)
         best_model_path = config.checkpoint_load_path
@@ -163,23 +242,23 @@ if __name__ == "__main__":
         # load the checkpoint
         model = ResFlow.load_from_checkpoint(best_model_path, map_location="cpu")
         model.to("cuda")
+    # train
+    printer("Starting training...")
+    trainer.fit(model, data_module)
 
-    # Train the model
-    if config.train.stage == "train":
-        printer("Starting training...")
-        trainer.fit(model, data_module)
+    if config.tune_hyperparameters is False:
         # best model path
         best_model_path = checkpointer.best_model_path
         printer(f"Best model saved at {best_model_path}")
-    elif config.train.stage == "eval":
-        printer("Starting evaluation...")
-        assert config.dataloader.reload is True, "Reload must be True for evaluation"
-        check_path(config.checkpoint_load_path)
-        best_model_path = config.checkpoint_load_path
+        return best_model_path, data_module
 
-    # Inference
+
+def infer_model(best_model_path, data_module):
+    # load the best model
     best_model = ResFlow.load_from_checkpoint(best_model_path, map_location="cpu")
     best_model.to("cuda" if torch.cuda.is_available() else "cpu")
+
+    # infer the mode
     infer = Inference(
         model=best_model,
         test_config=data_module.test_config,
@@ -188,4 +267,39 @@ if __name__ == "__main__":
         mfFlow=config.mfFlow,
         generate_config=config.generate,
     )
+
     infer()
+
+
+if __name__ == "__main__":
+    # print header
+    print_header(config)
+    # if tune hyperparameters is True, load the hyperparameter config
+    if config.tune_hyperparameters:
+        # load the hyperparameter config from a yaml file
+        with open("config_sweep.yml", "r") as file:
+            hp_config = yaml.safe_load(file)
+        # initialize agent
+        sweep_id = wandb.sweep(hp_config)
+        wandb.agent(sweep_id, function=train_model, count=1)
+    else:
+        # load the hyperparameter config
+        with open("config_hyperparameters.yml", "r") as file:
+            hp_config = yaml.safe_load(file)["FLOREN"]
+        # get the best model path (training or evaluation)
+        if config.train.stage == "train":
+            best_model_path, data_module = train_model(hp_config)
+        elif config.train.stage == "eval":
+            # TODO: Currently broken as it must take in the dataloader
+            raise NotImplementedError(
+                "Evaluation stage is not implemented yet.\
+                                      Dataloader is not available from the train_model\
+                                      function"
+            )
+            assert (
+                config.dataloader.reload is True
+            ), "Reload must be True for evaluation"
+            # check_path(config.checkpoint_load_path)
+            # best_model_path = config.checkpoint_load_path
+        # infer
+        infer_model(best_model_path, data_module)
