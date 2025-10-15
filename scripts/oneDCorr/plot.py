f2ee538f54186e2707d09aa64ff49bd95dd07893
sahilbhola14 <sbhola@umich.edu>
2025-10-15 16:14:32 -0400

    fix: E501

--- scripts/oneDCorr/plot.py
+++ scripts/oneDCorr/plot.py
@@ -1,7 +1,5 @@
-import numpy as np
 import torch
 import matplotlib.pyplot as plt
-from mpl_toolkits.axes_grid1.inset_locator import inset_axes, mark_inset
 
 plt.style.use("../journal.mplstyle")
 
@@ -12,9 +10,8 @@ plot_idx = {"automatic": True, "idx": 6}  # Set to True for automatic index sele
 # End user input
 file_floral = f"oneDCorr_{n_samples}_samples_results_floral.pt"
 file_flora = f"oneDCorr_{n_samples}_samples_results_flora.pt"
-print(
-    f"Loading results from {file_floral} and {file_flora} with {n_samples} samples."
-)
+print(f"Loading results from {file_floral} and {file_flora} with {n_samples} samples.")
+
 
 def find_best_idx(true, prediction):
     """
@@ -26,9 +23,13 @@ def find_best_idx(true, prediction):
     print(f"Best index found: {idx} with error {error.min().item()}")
     return idx
 
+
 def plot_field(data_flora, data_floral):
     if plot_idx["automatic"]:
-        idx = find_best_idx(true=data_floral["HF_field_plot"], prediction=data_floral["HF_field_prediction_plot"])
+        idx = find_best_idx(
+            true=data_floral["HF_field_plot"],
+            prediction=data_floral["HF_field_prediction_plot"],
+        )
     else:
         idx = plot_idx["idx"]
     print(f"Plotting index {idx}")
@@ -46,18 +47,37 @@ def plot_field(data_flora, data_floral):
     std_flora = HF_field_prediction_flora.std(dim=0).ravel()
     std_floral = HF_field_prediction_floral.std(dim=0).ravel()
 
-
-    fig, axs = plt.subplots(1, 2, figsize=(12, 5), constrained_layout=True, sharey=True, sharex=True)
+    fig, axs = plt.subplots(
+        1, 2, figsize=(12, 5), constrained_layout=True, sharey=True, sharex=True
+    )
     axs[0].plot(field_domain, HF_field, label="High-fidelity", color="black")
-    axs[0].plot(field_domain, LF_field, label="Low-fidelity", color="gray", linestyle="dashed")
+    axs[0].plot(
+        field_domain, LF_field, label="Low-fidelity", color="gray", linestyle="dashed"
+    )
 
     axs[1].plot(field_domain, HF_field, label="High-fidelity", color="black")
-    axs[1].plot(field_domain, mean_flora, label=r"$\mu_{FLORA}$", color="green", linestyle="--")
-    axs[1].fill_between(field_domain, mean_flora - sigma_factor * std_flora, mean_flora + sigma_factor * std_flora, color="green", alpha=0.3, label="")
-    # axs[1].fill_between(field_domain, mean_flora - sigma_factor * std_flora, mean_flora + sigma_factor * std_flora, color="green", alpha=0.3, label=rf"$\mu_{{FLORA}} \pm {sigma_factor} \sigma$")
-    axs[1].plot(field_domain, mean_floral, label=r"$\mu_{FLORAL}$", color="red", linestyle=":")
-    # axs[1].fill_between(field_domain, mean_floral - sigma_factor * std_floral, mean_floral + sigma_factor * std_floral, color="red", alpha=0.3, label=rf"$\mu_{{FLORAL}} \pm {sigma_factor} \sigma$")
-    axs[1].fill_between(field_domain, mean_floral - sigma_factor * std_floral, mean_floral + sigma_factor * std_floral, color="red", alpha=0.3, label="")
+    axs[1].plot(
+        field_domain, mean_flora, label=r"$\mu_{FLORA}$", color="green", linestyle="--"
+    )
+    axs[1].fill_between(
+        field_domain,
+        mean_flora - sigma_factor * std_flora,
+        mean_flora + sigma_factor * std_flora,
+        color="green",
+        alpha=0.3,
+        label="",
+    )
+    axs[1].plot(
+        field_domain, mean_floral, label=r"$\mu_{FLORAL}$", color="red", linestyle=":"
+    )
+    axs[1].fill_between(
+        field_domain,
+        mean_floral - sigma_factor * std_floral,
+        mean_floral + sigma_factor * std_floral,
+        color="red",
+        alpha=0.3,
+        label="",
+    )
 
     for ax in axs:
         ax.set_xlabel(r"$x_w$")
@@ -69,10 +89,9 @@ def plot_field(data_flora, data_floral):
     plt.savefig("oneD_field_comparison.png", dpi=300)
     plt.close()
 
+
 if __name__ == "__main__":
     data_flora = torch.load(file_flora, weights_only=False)
     data_floral = torch.load(file_floral, weights_only=False)
     # plot field
     plot_field(data_flora, data_floral)
-
-
