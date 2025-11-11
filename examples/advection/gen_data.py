89c03a73bc08eb3ac848068c6b2598cb5791b122
Sahil Bhola <sbhola@umich.edu>
2025-11-11 18:15:54 -0500

    chore: udpate base config

--- examples/advection/gen_data.py
+++ examples/advection/gen_data.py
@@ -1,10 +1,6 @@
 # examples/advection/gen_data.py
 """
 Advection equation
-Notes:
-    1. For default config, LF has cost savings of approx. 3.01 times.
-    6000 samples HF: 377.8 seconds
-    6000 samples LF: 377.8 seconds
 """
 import torch
 import numpy as np
