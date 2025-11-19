c00ac04ef5c605342f36aa06443d5a17fb0dcf61
Sahil Bhola <sbhola@umich.edu>
2025-11-19 17:07:00 -0500

    chore: update figsize

--- scripts/oneDCorr/plot.py
+++ scripts/oneDCorr/plot.py
@@ -8,7 +8,7 @@ import matplotlib.pyplot as plt
 from floral.utils import oneDPlot, ParetoPlot, ErrorSummary, BaseResidual
 
 # Begin user input
-n_train_samples_list = [50, 100, 1000, 5000]
+n_train_samples_list = [10, 50, 500, 1000]
 n_val_samples = 1000
 # End user input
 
@@ -61,7 +61,7 @@ def plot_field(n_train_samples):
     # create plot object
     plotter = oneDPlot(data_flora=data_flora, data_floral=data_floral)
     # create sample plot
-    plotter.make_field_sample_plot(std_factor=20)
+    plotter.make_field_sample_plot(std_factor=10)
 
 
 def plot_pareto(n_train_samples_list):
@@ -73,7 +73,7 @@ def plot_pareto(n_train_samples_list):
         df = ParetoPlot.get_pareto_data(data_flora=data_flora, data_floral=data_floral)
         all_data.append(df)
     combined_df = pd.concat(all_data, ignore_index=True)
-    ParetoPlot.plot_pareto(combined_df, ylim_range=(1e-2, 1e1))
+    ParetoPlot.plot_pareto(combined_df, ylim_range=(1e-2, 1e1), figsize=(7, 5))
 
 
 def plot_residual_summary(n_train_samples_list):
@@ -86,7 +86,7 @@ def plot_residual_summary(n_train_samples_list):
         df = residual(verbose=True)
         all_data.append(df)
     combined_df = pd.concat(all_data, ignore_index=True)
-    ResidualOneDCorr.plot_residual(combined_df)
+    ResidualOneDCorr.plot_residual(combined_df, figsize=(7, 5), xlim_range=(1e0, 1e4))
 
 
 def plot_error_summary(n_train_samples_list):
@@ -108,6 +108,7 @@ if __name__ == "__main__":
     # error summary
     plot_error_summary(n_train_samples_list)
     # # plot field
+    plot_field(n_train_samples=n_train_samples_list[-1])
     plot_field(n_train_samples=n_train_samples_list[0])
-    # # pareto
+    # pareto
     plot_pareto(n_train_samples_list)
