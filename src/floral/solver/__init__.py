9e787d3a610a9f6ada5e7e8c62629f1c9aa21a9c
Sahil Bhola <sbhola@umich.edu>
2025-12-01 13:05:35 -0500

    feat(darcy): Add solver

--- src/floral/solver/__init__.py
+++ src/floral/solver/__init__.py
@@ -1,9 +1,11 @@
 from .onedcorr import OneDCorr
 from .advection import Advection
 from .burgers import Burgers
+from .darcy import Darcy
 
 __all__ = [
     "OneDCorr",
     "Advection",
     "Burgers",
+    "Darcy",
 ]
