2ba0f7012ab822bf5649438bd4bef85dfd3a6b12
Sahil Bhola <sbhola@umich.edu>
2026-04-13 12:06:27 -0400

    chore: add group param to config

--- scripts/slurm/submit_superresolution.sh
+++ scripts/slurm/submit_superresolution.sh
@@ -8,10 +8,11 @@
 # OBJECTIVES=("fno" "fm")
 
 FLORAL_VALUES=(True False)
-N_TRAIN_SAMPLES_VALUES=(3500 1000 500 10) # same size as BATCH_SIZE
-BATCH_SIZE=(128 64 64 2) # same size as N_TRAIN_SAMPLES_VALUES
+N_TRAIN_SAMPLES_VALUES=10 # same size as BATCH_SIZE
+BATCH_SIZE=2 # same size as N_TRAIN_SAMPLES_VALUES
+TRAIN_RES=(8 16 32 64) # vary the train res
 OBJECTIVES=("fno" "fm")
-WANDB_GROUP="vary_training_size"
+WANDB_GROUP="superresolution"
 
 BASE_CONFIG="config"
 BASE_HP_CONFIG="hp_config"
@@ -21,15 +22,17 @@ LOG_FILE="ablation_submissions.log"
 # Loop over all combinations
 # -------------------------------
 for floral in "${FLORAL_VALUES[@]}"; do
-    for i in "${!N_TRAIN_SAMPLES_VALUES[@]}"; do
-    n_train=${N_TRAIN_SAMPLES_VALUES[$i]} # extract the train samples
-    batch_size=${BATCH_SIZE[$i]} # extact hte batch size
+    for train_res in "${TRAIN_RES[@]}"; do
+
+    n_train=${N_TRAIN_SAMPLES_VALUES} # extract the train samples
+    batch_size=${BATCH_SIZE} # extact the batch size
+
     for objective in "${OBJECTIVES[@]}"; do
 
-    EXP_NAME="floral_${floral}_ntrain_${n_train}_obj_${objective}"
+    EXP_NAME="floral_${floral}_ntrain_${n_train}_obj_${objective}_train_res_${train_res}"
 
     # One single override string
-    OVERRIDES="wandb_group=${WANDB_GROUP} floral=${floral} dataloader.n_train_samples=${n_train} train.objective=${objective}"
+    OVERRIDES="wandb_group=${WANDB_GROUP} floral=${floral} dataloader.n_train_samples=${n_train} train.objective=${objective} train.train_res=${train_res}"
 
     # Timestamp for logging
     TIMESTAMP=$(date "+%Y-%m-%d %H:%M:%S")
