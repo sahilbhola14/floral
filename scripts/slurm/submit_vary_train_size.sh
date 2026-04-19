e2f419afa1441936cccd946c3e10cd9da42b96be
Sahil Bhola <sbhola@umich.edu>
2026-04-19 14:12:11 -0400

    fix: update script

--- scripts/slurm/submit_vary_train_size.sh
+++ scripts/slurm/submit_vary_train_size.sh
@@ -8,8 +8,8 @@
 # OBJECTIVES=("fno" "fm")
 
 FLORAL_VALUES=(True False)
-N_TRAIN_SAMPLES_VALUES=(3500 1000 500 10) # same size as BATCH_SIZE
-BATCH_SIZE=(128 64 64 2) # same size as N_TRAIN_SAMPLES_VALUES
+N_UNIQUE_TRAIN_CONDITIONS=(2 4 10 20 40) # same size as BATCH_SIZE
+BATCH_SIZE=(32 32 32 32 32) # same size as N_TRAIN_SAMPLES_VALUES
 OBJECTIVES=("fno" "fm")
 WANDB_GROUP="vary_training_size"
 
@@ -20,16 +20,16 @@ LOG_FILE="ablation_submissions.log"
 # -------------------------------
 # Loop over all combinations
 # -------------------------------
-for i in "${!N_TRAIN_SAMPLES_VALUES[@]}"; do
-    n_train=${N_TRAIN_SAMPLES_VALUES[$i]} # extract the train samples
+for i in "${!N_UNIQUE_TRAIN_CONDITIONS[@]}"; do
+    n_unique_train=${N_UNIQUE_TRAIN_CONDITIONS[$i]} # extract the train samples
     batch_size=${BATCH_SIZE[$i]} # extact hte batch size
     for floral in "${FLORAL_VALUES[@]}"; do
     for objective in "${OBJECTIVES[@]}"; do
 
-    EXP_NAME="floral_${floral}_ntrain_${n_train}_obj_${objective}"
+    EXP_NAME="floral_${floral}_n_unique_train_${n_unique_train}_obj_${objective}"
 
     # One single override string
-    OVERRIDES="wandb_group=${WANDB_GROUP} floral=${floral} dataloader.n_train_samples=${n_train} train.objective=${objective}"
+    OVERRIDES="wandb_group=${WANDB_GROUP} floral=${floral} dataloader.n_unique_train_conditions=${n_unique_train} train.objective=${objective}"
 
     # Timestamp for logging
     TIMESTAMP=$(date "+%Y-%m-%d %H:%M:%S")
