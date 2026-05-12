10959fdfb1f896c2bcc2a4a0079fce4d787afb4e
Sahil Bhola <sbhola@umich.edu>
2026-05-12 09:46:06 -0400

    chore: update base plot

--- scripts/advection/plot.py
+++ scripts/advection/plot.py
@@ -231,12 +231,10 @@ def plot_residual_summary(n_train_samples_list):
 
 
 if __name__ == "__main__":
-    # residual summary
-    # plot_residual_summary(n_train_samples_list)
     # error summary
-    # plot_error_summary(n_train_samples_list)
+    plot_error_summary(n_train_samples_list)
     # # plot field
     plot_field(n_train_samples=n_train_samples_list[0])
     plot_field(n_train_samples=n_train_samples_list[-1])
     # pareto
-    # print_pareto(n_train_samples_list)
+    print_pareto(n_train_samples_list)
